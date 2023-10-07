5e174dbb37d525cb57965a13ce04f1a6f0b2ac83
al-ov71 <klever1987@yandex.ru>
2023-10-07 23:05:59 +0400

    mentors comments

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -5,7 +5,6 @@ from yaml import CLoader
 
 def parse(data, format):
     if format == 'json':
-        print(data)
         return json.loads(data)
     elif format == 'yaml':
         return yaml.load(data, Loader=CLoader)
